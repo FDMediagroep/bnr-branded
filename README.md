c5502ed6d6ab5592119e391aa7e7ceb3e743c8c7
srohde <sven.rohde@fdmediagroep.nl>
2021-03-09 11:16:08 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bnr-branded
+
+npm init
+npm i -D next react react-dom typescript @types/react @types/react-dom
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Styles from "./index.module.scss";
+
+function Page() {
+  return <h1 className={Styles.page}>Hello World</h1>;
+}
+
+export default Page;
