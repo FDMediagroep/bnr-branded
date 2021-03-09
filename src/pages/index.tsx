eee1d5356c1d0119057f48c41de12233d5d6fbe3
srohde <sven.rohde@fdmediagroep.nl>
2021-03-09 11:48:26 +0100

    fix config eslint prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import Styles from "./index.module.scss";
+import React from 'react';
+import Styles from './index.module.scss';
 
 function Page() {
-  return <h1 className={Styles.page}>Hello World</h1>;
+    return <h1 className={Styles.page}>Hello World</h1>;
 }
 
 export default Page;
