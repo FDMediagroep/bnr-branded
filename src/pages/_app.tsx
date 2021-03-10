62a6541d093d5c66578c478ba76444ca2c5cfbdb
Willem Liu <willem_liu@hotmail.com>
2021-03-10 16:41:18 +0100

    Added placeholder menu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,100 @@
 import React from 'react';
 import './_app.scss';
+import '@fdmg/bnr-design-system/components/menu/Menu.css';
+import {
+    BNRIcon,
+    SpyglassIcon,
+} from '@fdmg/bnr-design-system/design-tokens/icons';
+import { Menu } from '@fdmg/bnr-design-system/components/menu/Menu';
 import '@fdmg/bnr-design-system/components/design-tokens/design-tokens.css';
 import '@fdmg/css-grid/css/grid.css';
-import { Menu } from '../components/menu/Menu';
 import { Player } from '../components/player/Player';
+import styles from './_app.module.scss';
+import Link from 'next/link';
+import { Programs } from '../utils/omnyHelper';
 
-function Page({ Component, pageProps }) {
+const dummyFn = () => {
+    console.log('dummy');
+};
+
+interface PageProps {
+    Programs: Programs;
+    [x: string]: any;
+}
+
+function Page({
+    Component,
+    pageProps,
+}: {
+    Component: any;
+    pageProps: PageProps;
+}) {
     return (
         <>
-            <Menu Programs={pageProps.Programs} />
+            {/* <Menu Programs={pageProps.Programs} /> */}
+            <Menu
+                className={styles.menu}
+                ariaLabel="Main menu"
+                menuItems={[
+                    {
+                        component: (
+                            <Link href="/">
+                                <a style={{ padding: '0' }}>
+                                    <span
+                                        className={styles.logo}
+                                        dangerouslySetInnerHTML={{
+                                            __html: BNRIcon,
+                                        }}
+                                        aria-label="BNR Branded Podcasts"
+                                    />
+                                </a>
+                            </Link>
+                        ),
+                    },
+                    {
+                        text: 'Podcasts',
+                        isToggle: true,
+                        menuItems: [
+                            ...(pageProps?.Programs?.Programs?.map(
+                                (program) => {
+                                    return {
+                                        component: (
+                                            <Link
+                                                key={program.Id}
+                                                href={`/program/${program.Slug}`}
+                                            >
+                                                <a>{program.Name}</a>
+                                            </Link>
+                                        ),
+                                    };
+                                }
+                            ) ?? []),
+                        ],
+                    },
+                ]}
+            >
+                <>
+                    <form method="GET" action="/search" onSubmit={dummyFn}>
+                        <div className={styles.search}>
+                            <input
+                                name="q"
+                                placeholder="Search..."
+                                onBlur={dummyFn}
+                                aria-label="Search text"
+                            />
+                            <button
+                                type="submit"
+                                name="search"
+                                dangerouslySetInnerHTML={{
+                                    __html: SpyglassIcon,
+                                }}
+                                aria-label="Search submit"
+                            />
+                        </div>
+                    </form>
+                </>
+            </Menu>
+
             <Component {...pageProps} />
             <Player url={pageProps?.audioUrl} />
         </>
