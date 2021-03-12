8248deee94e979d8c3ccca8c69ee30c322198623
Willem Liu <willem_liu@hotmail.com>
2021-03-12 10:22:21 +0100

    Removed sanity env from next.config.js again

--- next.config.js
+++ next.config.js
@@ -11,9 +11,4 @@ module.exports = {
             'cdn.sanity.io',
         ],
     },
-    env: {
-        SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
-        SANITY_TOKEN: process.env.SANITY_TOKEN,
-        SANITY_DATASET: process.env.SANITY_DATASET,
-    },
 };
