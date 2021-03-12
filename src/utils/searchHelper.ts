5bbd0a32088fb957e147dd1bd4a04e8473bc1d17
Willem Liu <willem_liu@hotmail.com>
2021-03-12 18:34:23 +0100

    CLOUDSEARCH_SEARCH_URL env var

--- src/utils/searchHelper.ts
+++ src/utils/searchHelper.ts
@@ -4,7 +4,7 @@ export async function search(
     size = 25
 ): Promise<any> {
     return await fetch(
-        `https://search-bnrbranded-bge7q2xd2ruqvahh4flgrkqtdq.eu-west-1.cloudsearch.amazonaws.com/2013-01-01/search?q=${encodeURIComponent(
+        `${process.env.CLOUDSEARCH_SEARCH_URL}?q=${encodeURIComponent(
             query
         )}&size=${size}&start=${start}`
     ).then((res) => res.json());
