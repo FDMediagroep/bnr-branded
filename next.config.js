b7790c2787c49554b690afeb30c63abe76eed726
Willem Liu <willem_liu@hotmail.com>
2021-03-12 09:52:47 +0100

    Added SANITY env vars to next.config.js

--- next.config.js
+++ next.config.js
@@ -11,4 +11,9 @@ module.exports = {
             'cdn.sanity.io',
         ],
     },
+    env: {
+        SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+        SANITY_TOKEN: process.env.SANITY_TOKEN,
+        SANITY_DATASET: process.env.SANITY_DATASET,
+    },
 };
