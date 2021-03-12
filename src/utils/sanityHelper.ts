031c239872bd5b55c12ae1a4f87167f31479103c
Willem Liu <willem_liu@hotmail.com>
2021-03-12 09:46:14 +0100

    logging

--- src/utils/sanityHelper.ts
+++ src/utils/sanityHelper.ts
@@ -7,7 +7,7 @@ export const client = sanityClient({
     projectId: process.env.SANITY_PROJECT_ID,
     dataset: process.env.SANITY_DATASET,
     token: process.env.SANITY_TOKEN, // or leave blank to be anonymous user
-    useCdn: true, // `false` if you want to ensure fresh data
+    useCdn: false, // `false` if you want to ensure fresh data
 });
 
 export async function getProgramEnrichment(
@@ -24,6 +24,7 @@ export async function getProgramEnrichment(
             }`,
         params
     );
+    console.log('SANITY getProgramEnrichment:', programId, result);
     return result?.result?.length && result?.result[0]?.color
         ? result.result[0]
         : { color: '#ffffff', sponsors: [] };
