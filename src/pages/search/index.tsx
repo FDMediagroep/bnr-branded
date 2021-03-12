59e134a1a53182fb3a5a7e0e6c0870081df8acc6
Willem Liu <willem_liu@hotmail.com>
2021-03-12 17:09:32 +0100

    Added search pagination

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -2,6 +2,11 @@ import '@fdmg/bnr-design-system/components/card/HorizontalCard1.css';
 import { HorizontalCard1 } from '@fdmg/bnr-design-system/components/card/HorizontalCard1';
 import React from 'react';
 import { search } from '../../utils/searchHelper';
+import Link from 'next/link';
+import '@fdmg/bnr-design-system/components/button/ButtonGhost.css';
+import { ButtonGhost } from '@fdmg/bnr-design-system/components/button/ButtonGhost';
+import styles from './Search.module.scss';
+
 interface Props {
     q?: string;
     hits: any;
@@ -9,16 +14,48 @@ interface Props {
 
 function Page(props: Props) {
     console.log(props.hits);
+
+    function hasNext() {
+        return props.hits.hits.found - props.hits.hits.start > 25;
+    }
+
     return props.hits ? (
-        <section className="default-content-body grid">
+        <section className={`${styles.search} default-content-body grid`}>
             <main className="xs-12 m-8 l-9">
                 {props.hits.hits.found ? (
-                    <h1 className="heading sans l">
+                    <h1 className={`${styles.header} heading sans l`}>
                         {props.hits.hits.found} Resultaten
                     </h1>
                 ) : (
-                    <h1 className="heading sans l">Geen resultaten</h1>
+                    <h1 className={`${styles.header} heading sans l`}>
+                        Geen resultaten
+                    </h1>
                 )}
+
+                {props.hits.hits.found > 25 ? (
+                    <section className={styles.pagination}>
+                        <Link
+                            href={`/search?q=${props.q}&start=${Math.max(
+                                props.hits.hits.start - 25,
+                                0
+                            )}`}
+                        >
+                            <ButtonGhost disabled={props.hits.hits.start == 0}>
+                                &lt;
+                            </ButtonGhost>
+                        </Link>
+                        <Link
+                            href={`/search?q=${props.q}&start=${
+                                props.hits.hits.start + 25
+                            }`}
+                        >
+                            <ButtonGhost disabled={!hasNext()}>
+                                &gt;
+                            </ButtonGhost>
+                        </Link>
+                    </section>
+                ) : null}
+
                 {props.hits.hits.hit.map((hit) => {
                     return (
                         <HorizontalCard1
@@ -38,8 +75,9 @@ function Page(props: Props) {
 
 export async function getServerSideProps(ctx) {
     const q = ctx.query.q || 'dog|-dog';
-    const hits = await search(q);
-    return { props: { q, hits: hits } };
+    const start = ctx.query.start || 0;
+    const hits = await search(q, start);
+    return { props: { q, start, hits: hits } };
 }
 
 export default Page;
--- src/utils/searchHelper.ts
+++ src/utils/searchHelper.ts
@@ -1,7 +1,11 @@
-export async function search(query: string): Promise<any> {
+export async function search(
+    query: string,
+    start = 0,
+    size = 25
+): Promise<any> {
     return await fetch(
         `https://search-bnrbranded-bge7q2xd2ruqvahh4flgrkqtdq.eu-west-1.cloudsearch.amazonaws.com/2013-01-01/search?q=${encodeURIComponent(
             query
-        )}&size=25&start=0`
+        )}&size=${size}&start=${start}`
     ).then((res) => res.json());
 }
