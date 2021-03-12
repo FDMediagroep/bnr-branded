717286007e2b1d3e7ab3213091dd7ac11cd367d6
Willem Liu <willem_liu@hotmail.com>
2021-03-12 16:49:10 +0100

    Search all as default

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,6 +1,9 @@
+import '@fdmg/bnr-design-system/components/card/HorizontalCard1.css';
+import { HorizontalCard1 } from '@fdmg/bnr-design-system/components/card/HorizontalCard1';
 import React from 'react';
 import { search } from '../../utils/searchHelper';
 interface Props {
+    q?: string;
     hits: any;
 }
 
@@ -8,11 +11,17 @@ function Page(props: Props) {
     return props.hits ? (
         <section className="default-content-body grid">
             <main className="xs-12 m-9">
-                <ul>
-                    {props.hits.hits.hit.map((hit) => {
-                        return <li key={hit.id}>{hit.fields.title}</li>;
-                    })}
-                </ul>
+                {props.hits.hits.hit.map((hit) => {
+                    return (
+                        <HorizontalCard1
+                            key={hit.id}
+                            id={hit.id}
+                            title={hit.fields.title}
+                            href={`/search?q=${props.q}`}
+                            label={props.hits.fields?.episode ?? null}
+                        />
+                    );
+                })}
             </main>
             <aside className="xs-12 m-3">ASIDE</aside>
         </section>
@@ -20,9 +29,9 @@ function Page(props: Props) {
 }
 
 export async function getServerSideProps(ctx) {
-    const q = ctx.query.q ?? '*';
+    const q = ctx.query.q ?? 'dog|-dog';
     const hits = await search(q);
-    return { props: { hits: hits } };
+    return { props: { q, hits: hits } };
 }
 
 export default Page;
