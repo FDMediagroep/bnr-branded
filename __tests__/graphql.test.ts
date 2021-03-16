3b49b318bc4c5212423494f97f369189fad69af1
Willem Liu <willem_liu@hotmail.com>
2021-03-16 14:20:23 +0100

    Added schema breaking change unit test

--- __tests__/graphql.test.ts
+++ __tests__/graphql.test.ts
@@ -4,6 +4,9 @@ import { omnyTypeDefs } from '../src/graphql/omny/schemas';
 import { sanityTypeDefs } from '../src/graphql/sanity/schemas';
 import { omnyResolvers } from '../src/graphql/omny/resolvers';
 import { sanityResolvers } from '../src/graphql/sanity/resolvers';
+import { loadSchema } from '@graphql-tools/load';
+import { UrlLoader } from '@graphql-tools/url-loader';
+import { diff } from '@graphql-inspector/core';
 
 describe('GraphQL', () => {
     const typeDefs = mergeTypeDefs([omnyTypeDefs, sanityTypeDefs]);
@@ -12,8 +15,23 @@ describe('GraphQL', () => {
         typeDefs,
         resolvers,
     });
+    let remoteSchema;
+
+    beforeAll(async () => {
+        remoteSchema = await loadSchema(
+            'https://bnr-branded.vercel.app/api/graphql',
+            {
+                // load from endpoint
+                loaders: [new UrlLoader()],
+            }
+        );
+    });
 
     test('schema is backward compatible', () => {
-        expect(executableSchema).toMatchSnapshot();
+        const changes = diff(remoteSchema, executableSchema);
+        const breaking = changes.find(
+            (change) => change.criticality.level === 'BREAKING'
+        );
+        expect(breaking).toBeFalsy();
     });
 });
