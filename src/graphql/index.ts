067607eb8dec2a1b50fc38e4c133ca481827b15e
Willem Liu <willem_liu@hotmail.com>
2021-03-26 13:16:07 +0100

    doc

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -8,8 +8,8 @@ import sanityTypes from './sanity/schemas.gql';
 export const typeDefs = mergeTypeDefs([omnyTypes, sanityTypes]);
 export const resolvers = mergeResolvers([omnyResolvers, sanityResolvers]);
 /**
- * Use makeExecutableSchema from graphql-tools instead of buildSchema from graphql.js
- * It's more flexible and allows for field-specific resolving of queries.
+ * makeExecutableSchema from graphql-tools instead of buildSchema from graphql.js
+ * It's more flexible and allows for field resolvers.
  */
 export const executableSchema = makeExecutableSchema({
     typeDefs,
