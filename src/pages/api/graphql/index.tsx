f858d6d39d60137aba31b5d4a42033e05a8270f0
Willem Liu <willem_liu@hotmail.com>
2021-03-29 12:31:36 +0200

    Removed unnecessary cache headers

--- src/pages/api/graphql/index.tsx
+++ src/pages/api/graphql/index.tsx
@@ -3,11 +3,6 @@ import { graphqlHTTP } from 'express-graphql';
 import { executableSchema } from '../../../graphql';
 
 async function handler(req, res) {
-    res?.setHeader?.(
-        'Cache-Control',
-        'max-age=0, max-stale, s-maxage=60, stale-while-revalidate'
-    );
-
     return await graphqlHTTP({
         schema: executableSchema,
         validationRules: [depthLimit(4)],
